2ffef7ec13d380a5c8dd70e4a032d019300abbc4
E5note <xuzhuoli@outlook.com>
2025-06-07 22:43:37 +0800

    Site updated: 2025-06-07 22:43:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/2.html","posts/1.html"];function toRandomPost(){
+var posts=["posts/62142.html","posts/27630.html","posts/29402.html","posts/3083.html","posts/26403.html","posts/22073.html","posts/26480.html","posts/44647.html","posts/26479.html","posts/8.html","posts/38994.html","posts/41657.html","posts/26479.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
